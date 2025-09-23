c0cad3ed2b0923002e3e9fb61c650423c94ffa3d
Rameen Burdabar <181733084+Rameen-dev@users.noreply.github.com>
2025-09-23 15:43:20 +0100

    Updated Variables.cs with practice code

--- Variables.cs
+++ Variables.cs
@@ -5,6 +5,30 @@ public static class Variables
 {
     public static void Run()
     {
-        Console.WriteLine("Variables program running");
+        Console.WriteLine("Variables Practice");
+
+        // Integer (Whole Number)
+
+        int age = 17;
+        Console.WriteLine($"Your age is {age}");
+
+        // Update the value of the variable 
+
+        age = age + 1;
+        Console.WriteLine($"Next year you will be {age}");
+
+        // Double (Decimal number)
+
+        double price = 19.99;
+        Console.WriteLine($"The total cost of 1 Item is {price}");
+
+        // Do a calculation with double values
+        double total = price * 3;
+        Console.WriteLine($"The total cost for 3 Items is {total}");
+
+        // String (Text)
+
+        string name = "Rameen";
+        Console.WriteLine($"My name is {name}");
     }
 }
