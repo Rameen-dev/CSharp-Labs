43156dcaa57ac537f8e0c89354881d06726e86ff
Rameen Burdabar <181733084+Rameen-dev@users.noreply.github.com>
2025-09-24 11:59:40 +0100

    Completed Week1 Task Worksheet - Warm Up 1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ while (true) // Loop forever until the user chooses "Exit"
     // Display the menu options
     Console.WriteLine("\n=== C# Labs ===");   // Menu Title
 
+
     Console.WriteLine("1) Variables");        // Option 1
     Console.WriteLine("0) Exit");             // Exit Program
+
+    Console.WriteLine("Week 1");
 
     Console.Write("Choices: ");
 
@@ -19,6 +22,9 @@ while (true) // Loop forever until the user chooses "Exit"
     {
         case "1":
             Variables.Run();
+            break;
+        case "Week 1":
+            Week1_WarmUp.WarmUp1(); 
             break;
 
         case "0":
---
+++ Week1_WarmUp.cs
@@ -0,0 +1,53 @@
+﻿using CSharpLab;
+
+public static class Week1_WarmUp
+{
+    public static void Run()
+    {
+        Console.WriteLine("Week 1 is running");
+    }
+    public static void WarmUp1()
+    {
+        Console.Write("Enter the first number for the Clamp function: ");
+        int value = ReadIntUserInput();
+
+        Console.Write("Enter the second number for the Clamp function: ");
+        int min = ReadIntUserInput();
+
+        Console.Write("Enter the third number for the Clamp function: ");
+        int max = ReadIntUserInput();
+
+        int result = Clamp(value, min, max);
+
+        Console.WriteLine($"Clamped result is: {result}");
+
+    }
+
+    private static int Clamp(int value, int min, int max)
+    {
+        if (value < min)
+            return min;
+
+        if (value > max)
+            return max;
+
+        return value;
+    }
+        
+
+    private static int ReadIntUserInput()
+    {
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+                return number;
+
+            Console.WriteLine("Please enter a valid number");
+        }
+    }
+}
+  
+
+
+   
