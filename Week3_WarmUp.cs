1aa6b90f6c969507fea36ac21ee625f7f27ec81b
Rameen Burdabar <181733084+Rameen-dev@users.noreply.github.com>
2025-10-29 11:00:36 +0000

    Clean Up

--- Week3_WarmUp.cs
+++ Week3_WarmUp.cs
@@ -23,6 +23,7 @@ public static class Week3_WarmUp
 
     public static void WarmUp2()
     {
+        // Create a list of words (collection) - Each word is a string.
         List<string> words = new List<string>
         {
             "Dog",
@@ -31,9 +32,11 @@ public static class Week3_WarmUp
             "Airplane",
             "Animal",
         };
+        // Here I use the Where() to filter through the list
+        // Take each word, and return true if its length is less than or equal to 5 characters.
+        IEnumerable<string> shortwords = words.Where(word => word.Length  <= 5); // The lambda function returns TRUE or FALSE for each item.
 
-        IEnumerable<string> shortwords = words.Where(word=> word.Length  <= 5);
-
+        // Below is just a clean simple way of looping through the words in the collection, and listing them in a neat manner.
         Console.WriteLine("Words shorter than 5 characters:\n ");
 
         int count = 1;
@@ -43,7 +46,5 @@ public static class Week3_WarmUp
             count++;
         }
         Console.ReadLine();
-
     }
-
 }
